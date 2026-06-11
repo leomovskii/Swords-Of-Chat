0ade6a5ee9111f8fe6db551f1dd9f3883a69d648
Bohdan <leomovskii@gmail.com>
2026-06-11 03:07:55 +0300

    Prototype item structure

---
+++ Game/Inventory/ItemData.cs
@@ -0,0 +1,9 @@
+﻿namespace SwordsOfChat.Game.Inventory {
+	internal struct ItemData {
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public float Weight { get; set; }
+		public Dictionary<string, int> Effects { get; set; }
+	}
+}
